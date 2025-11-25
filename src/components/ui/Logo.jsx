29cc9c5ef04b1df247336128c55205b83dd54527
Yousef Balaawi <turbo@192.168.1.105>
2025-11-26 02:13:38 +0300

    Increased logo size to h-40

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,9 +1,19 @@
-export default function Logo({ className = "w-64" }) {
+import React from 'react';
+// 1. IMPORT the file from the assets folder
+import logoUrl from '../../assets/logo.svg';
+
+const Logo = () => {
   return (
-    <img 
-      src="/logo.svg" 
-      alt="Keyframe Global" 
-      className={`${className} select-none`}
-    />
+    <div className="logo-container">
+      {/* 2. Use the variable 'logoUrl' instead of a string path */}
+      <img 
+        src={logoUrl} 
+        alt="Keyframe Kinetic" 
+        // Changed to h-32 (128px). Try h-40 or h-[200px] if still too small.
+        className="h-40 w-auto" 
+      />
+    </div>
   );
-}
+};
+
+export default Logo;
